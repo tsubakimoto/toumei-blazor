6977195ec1f0ca5b17fd8b814c3a044ac2688ad3
tkmbmbr <tkmbmbr@gmail.com>
2021-04-11 22:22:16 +0900

    Update blazor form

---
+++ src/Models/ColorModel.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace toumei.Models
+{
+    public class ColorModel
+    {
+        [Required]
+        [StringLength(6)]
+        public string Color { get; set; } = "FFFFFF";
+    }
+}
